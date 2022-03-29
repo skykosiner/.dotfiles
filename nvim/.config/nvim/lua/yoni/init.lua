7f094439f172dbfaa8b7ea4bb75658eb4b833ca2
Yoni Kosiner <yoni@kosiner.co.uk>
2022-03-29 23:11:08 +0100

    Give it the D

--- nvim/.config/nvim/lua/yoni/init.lua
+++ nvim/.config/nvim/lua/yoni/init.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-vim.g.netrw_localrmdir='rm -r'
+vim.g.netrw_localrmdir='rm -rf'
 
 -- I'll put this in lua one day
 vim.cmd([[
