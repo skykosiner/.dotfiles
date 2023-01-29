fdfa08e87cd0c396b48dc3067837aecaa0694cc1
Sky Kosiner <yoni@kosiner.co.uk>
2023-01-29 18:29:33 +0000

    feat: even more spotify stuff

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 * I use uwuntu and gnome-terminal btw - What a loser using uwuntu and gnome-terminal, I now use arch btw with kitty btw
 
 ![Image of my vim and stuff](./media/workflow)
+
+## Personal note
+* To update spotify albums use [this](https://developer.spotify.com/web-api/console/get-current-user-saved-albums/)
---
+++ nvim/.config/nvim/after/plugin/firenvim.lua
@@ -0,0 +1,28 @@
+vim.cmd([[
+let g:firenvim_config = {
+            \ 'globalSettings': {
+            \ 'alt': 'all',
+            \  },
+            \ 'localSettings': {
+            \ '.*': {
+            \ 'cmdline': 'neovim',
+            \ 'content': 'text',
+            \ 'priority': 0,
+            \ 'selector': 'textarea',
+            \ 'takeover': 'always',
+            \ },
+            \ }
+            \ }
+
+let fc = g:firenvim_config['localSettings']
+let fc['https?://twitter.com'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://www.twitch.tv'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://dashboard.twitch.tv'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://www.instagram.com'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://www.notion.so'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://roamresearch.com'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://youtube.com'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://replit.com'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://interhigh.adobeconnect.com'] = {'takeover': 'never', 'priority': 1}
+let fc['https?://discord.com'] = {'takeover': 'never', 'priority': 1}
+]])
