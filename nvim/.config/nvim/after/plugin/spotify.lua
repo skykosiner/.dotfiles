15422c71133a6521bd082db98b277ee549e9db35
Sky Kosiner <yoni@kosiner.co.uk>
2023-01-30 01:38:54 +0000

    feat: yeah i can control spotify from vim. idk about yo

--- nvim/.config/nvim/after/plugin/spotify.lua
+++ nvim/.config/nvim/after/plugin/spotify.lua
@@ -1,40 +1,43 @@
-function string.split(inputstr, sep)
-  if sep == nil then
-    sep = "%s"
-  end
-  local t = {}
-  for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
-    table.insert(t, str)
-  end
-  return t
-end
+local function play_playlist(prompt_bufnr, map)
+  local function run_me_daddy(close)
+    local content = require("telescope.actions.state").get_selected_entry(
+      prompt_bufnr
+    )
 
-local function play_playlist(prompt_bufnr)
-  local content = require("telescope.actions.state").get_selected_entry(
-    prompt_bufnr
-  )
+    if close then
+      require("telescope.actions").close(prompt_bufnr)
+    end
 
-  require("telescope.actions").close(prompt_bufnr)
+    local id = vim.fn.systemlist(string.format([[ echo "%s" | awk -F 'id: ' '{print $2}' ]], content.value))[1]
 
-  local id = string.split(content.value, "id :")
-  vim.fn.system(string.format([[
+    vim.fn.system(string.format([[
   qdbus org.mpris.MediaPlayer2.spotify \
   /org/mpris/MediaPlayer2 \
   org.mpris.MediaPlayer2.Player.OpenUri \
   spotify:album:%s
-  ]], id[#id]))
+  ]] , id))
+  end
+
+  map("i", "<C-s>", function()
+    run_me_daddy(false)
+  end)
+
+  map("i", "<CR>", function()
+    run_me_daddy(true)
+  end)
 end
 
 local function select_song()
   local albums = vim.fn.systemlist([[ cat ~/.dotfiles/album_list | awk -F '|' '{print $1 " - "  $2 " id: " $3}']])
   require("telescope.pickers").new({}, {
-    prompt_title = "Chose playlis",
+    prompt_title = "< Chose Album >",
     finder = require("telescope.finders").new_table({
       results = albums
     }),
     sorter = require("telescope.config").values.generic_sorter({}),
-    attach_mappings = function(_, map)
-      map("i", "<CR>", play_playlist)
+    attach_mappings = function(prompt_bufnr, map)
+      play_playlist(prompt_bufnr, map)
+
       return true
     end,
   }):find()
