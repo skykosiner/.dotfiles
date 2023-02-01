c4d4d04b25d41811d76c0c813b278de9ed2fa314
Sky Kosiner <yoni@kosiner.co.uk>
2023-02-01 00:55:59 +0000

    Trying out the darkside, emacs??????

---
+++ doom-emacs/.doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/org/school/english/tutor/Tutor.org" "/home/yoni/org/school/film-studies/NEA.org" "/home/yoni/org/test.org" "/home/yoni/org-basics.org"))
+ '(package-selected-packages '(undo-tree)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
