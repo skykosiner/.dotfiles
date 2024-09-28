7d26f5ee77a71f428b37aec4b2726414f298d29e
Sky Kosiner <sky@skykosiner.com>
2024-09-28 18:18:50 +0100

    commit it baby

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,5 +1,3 @@
-vim.cmd.colorscheme("rose-pine-main")
-
 vim.cmd("highlight ColorColumn ctermbg=0 guibg=#555555")
 vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none", fg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
@@ -7,3 +5,4 @@ vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#9EBDE8" })
 vim.api.nvim_set_hl(0, "CmpNormal", { bg = "#000000" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.cmd.colorscheme("rose-pine-main")
