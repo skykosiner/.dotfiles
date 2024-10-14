db53bfbbf622f6ce75eeb55f600bee53ed00d7c4
Sky Kosiner <sky@skykosiner.com>
2024-10-14 15:03:59 +0100

    markdown.nvim

---
+++ nvim/.config/nvim/lua/sky/lazy/markdown.lua
@@ -0,0 +1,14 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+    },
+    ---@module "render-markdown"
+    ---@type render.md.UserConfig
+    opts = {
+        heading = {
+            width = "block",
+        }
+    },
+}
