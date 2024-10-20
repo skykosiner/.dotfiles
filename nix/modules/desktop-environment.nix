7a10d2adca6d77b91c9ec64a60829bd17fe424ab
Sky Kosiner <sky@skykosiner.com>
2024-10-20 14:57:56 +0100

    Becoming a nixos pro??

--- nix/modules/desktop-environment.nix
+++ nix/modules/desktop-environment.nix
@@ -3,8 +3,10 @@
 {
   home.packages = with pkgs; [
       swww
+      libreoffice
       waybar
       hyprland
+      zathura
       rofi
       grim
       slurp
@@ -14,5 +16,26 @@
     enable = true;
     font = "JetBrainsMono Nerd Font 14";
     theme = "/home/sky/.nix-profile/share/rofi/themes/solarized.rasi";
+  };
+
+  programs.zathura = {
+      enable = true;
+      extraConfig = ''
+        set sandbox none
+        set statusbar-h-padding 0
+        set statusbar-v-padding 0
+        set page-padding 1
+        set selection-clipboard clipboard
+        map u scroll half-up
+        map d scroll half-down
+        map D toggle_page_mode
+        map r reload
+        map R rotate
+        map K zoom in
+        map J zoom out
+        map i recolor
+        map p print
+        map g goto top
+      '';
   };
 }
--- nix/modules/development.nix
+++ nix/modules/development.nix
@@ -1,40 +1,47 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    alacritty
+    home.packages = with pkgs; [
+        alacritty
 
-    # Language stuff
-    python3
-    nodejs
-    go
+        # Language stuff
+        python3
+        nodejs
+        go
 
-    # Make stuff
-    cmake
-    ninja
-    gcc
-    gnumake
+        # Make stuff
+        cmake
+        ninja
+        gcc
+        gnumake
 
-    # Tools
-    git
-    zsh
-    tmux
-    fzf
-  ];
+        # Tools
+        git
+        zsh
+        tmux
+        fzf
+    ];
 
-  programs.alacritty = {
-    enable = true;
-    settings = {
-        window.opacity = 0.8;
-        colors.primary.background = "#000000";
-    	font.size = 16;
-        font.normal.family = "JetBrainsMono Nerd Font";
+    programs.git = {
+        enable = true;
+        userName = "Sky Kosiner";
+        userEmail = "sky@skykosiner.com";
     };
-  };
 
-  # You can also set up other configurations specific to development
-  # For example, environment variables or tools
-  home.sessionVariables = {
-    NODE_ENV = "development";
-  };
+
+    programs.alacritty = {
+        enable = true;
+        settings = {
+            window.opacity = 0.8;
+            colors.primary.background = "#000000";
+            font.size = 16;
+            font.normal.family = "JetBrainsMono Nerd Font";
+        };
+    };
+
+    # You can also set up other configurations specific to development
+    # For example, environment variables or tools
+    home.sessionVariables = {
+        NODE_ENV = "development";
+    };
 }
