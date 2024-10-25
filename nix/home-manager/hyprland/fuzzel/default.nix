228d385749ebab2a51e8dd090751460889e4850f
Sky Kosiner <sky@skykosiner.com>
2024-10-25 12:08:23 +0100

    fuzzel

--- nix/home-manager/hyprland/fuzzel/default.nix
+++ nix/home-manager/hyprland/fuzzel/default.nix
@@ -8,9 +8,9 @@
     programs.fuzzel = {
         enable = true;
         settings = {
-            colors.background = "3D4455AA";
+            colors.background = "3D4455DD";
             main = {
-                font = "Berkeley Mono:size=16";
+                font = "Berkeley Mono:size=10";
                 lines = 16;
                 terminal = "alacritty";
                 tabs = 2;
