7a3c151a09c239e9d7117c524a2dd1a6d89ca1bd
Sky Kosiner <sky@skykosiner.com>
2024-11-06 05:54:39 +0000

    update

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -25,28 +25,33 @@ in {
         };
     };
 
-    boot.kernelModules = [ "v4l2loopback" ];
-    boot.extraModulePackages = [ pkgs.linuxPackages.v4l2loopback ];
+    boot = {
+        kernelModules = [ "v4l2loopback" ];
+        extraModulePackages = [ pkgs.linuxPackages.v4l2loopback ];
 
-    # Bootloader
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+        loader = {
+            systemd-boot.enable = true;
+            efi.canTouchEfiVariables = true;
+        };
+    };
 
     networking.networkmanager.enable = true;
 
     time.timeZone = "Europe/London";
 
-    i18n.defaultLocale = "en_GB.UTF-8";
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_GB.UTF-8";
-        LC_IDENTIFICATION = "en_GB.UTF-8";
-        LC_MEASUREMENT = "en_GB.UTF-8";
-        LC_MONETARY = "en_GB.UTF-8";
-        LC_NAME = "en_GB.UTF-8";
-        LC_NUMERIC = "en_GB.UTF-8";
-        LC_PAPER = "en_GB.UTF-8";
-        LC_TELEPHONE = "en_GB.UTF-8";
-        LC_TIME = "en_GB.UTF-8";
+    i18n = {
+        defaultLocale = "en_GB.UTF-8";
+        extraLocaleSettings = {
+            LC_ADDRESS = "en_GB.UTF-8";
+            LC_IDENTIFICATION = "en_GB.UTF-8";
+            LC_MEASUREMENT = "en_GB.UTF-8";
+            LC_MONETARY = "en_GB.UTF-8";
+            LC_NAME = "en_GB.UTF-8";
+            LC_NUMERIC = "en_GB.UTF-8";
+            LC_PAPER = "en_GB.UTF-8";
+            LC_TELEPHONE = "en_GB.UTF-8";
+            LC_TIME = "en_GB.UTF-8";
+        };
     };
 
     services.xserver.enable = true;
