96a32ad2f8d5feb7950181c6a88618c6a80a9ec0
Sky Kosiner <sky@skykosiner.com>
2024-12-02 12:41:14 +0000

    colors and fonts

--- nix/home-manager/apps/alacritty/default.nix
+++ nix/home-manager/apps/alacritty/default.nix
@@ -20,7 +20,8 @@ in {
             # colors.primary.background = "#2A2F3A";
             colors.primary.background = "#000000";
             font.size = fontsize;
-            font.normal.family = "Berkeley Mono";
+            # font.normal.family = "Berkeley Mono";
+            font.normal.family = "JetbrainsMono Nerd Font";
         };
     };
 }
