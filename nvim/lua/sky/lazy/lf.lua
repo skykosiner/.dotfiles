9f66d6c51022a299a49cade05d84fc94aacf0c2f
Sky Kosiner <sky@skykosiner.com>
2024-12-28 10:26:18 +0000

    feat: lf changes

--- nvim/lua/sky/lazy/lf.lua
+++ nvim/lua/sky/lazy/lf.lua
@@ -6,6 +6,8 @@ return {
     config = function()
         vim.g.lf_map_keys = 0
         vim.g.floaterm_opener = "e"
+        vim.g.floaterm_width = 100
+        vim.g.floaterm_height = 30
         vim.keymap.set("n", "<M-o>", "<Cmd>Lf<CR>")
     end,
 }
