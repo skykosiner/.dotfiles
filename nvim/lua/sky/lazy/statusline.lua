9bc77701ff847e3ae6297d90194df288824afe56
Sky Kosiner <sky@skykosiner.com>
2024-12-29 19:47:44 +0000

    statusline fixed

--- nvim/lua/sky/lazy/statusline.lua
+++ nvim/lua/sky/lazy/statusline.lua
@@ -8,13 +8,13 @@ return {
         config = function()
             vim.opt.laststatus = 3
 
-            require("statusline").setup {
-                background_color = "#2e2e2e",
-                foreground_color = "#7fa3c0",
-                lsp_info = true,
-                harpoon_info = true,
-                git_info = true,
-                show_icons = true,
+            require("statusline"):setup {
+                background_color = "#7fa3c0",
+                foreground_color = "#2e2e2e",
+                lsp_info         = true,
+                harpoon_info     = true,
+                git_info         = true,
+                show_icons       = true,
             }
         end
     }
