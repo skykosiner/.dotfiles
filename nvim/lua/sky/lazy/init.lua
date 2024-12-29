a4c43c6131635234fecce9ba519997c59a841e47
Sky Kosiner <sky@skykosiner.com>
2024-12-29 11:22:17 +0000

    toggl stuff

--- nvim/lua/sky/lazy/init.lua
+++ nvim/lua/sky/lazy/init.lua
@@ -1,11 +1,5 @@
 return {
     {
         "dhruvasagar/vim-zoom"
-    },
-    {
-        dir = "~/personal/todo-me-daddy/redo/",
-        config = function()
-            local todos = require "todo-comments"
-        end
     }
 }
