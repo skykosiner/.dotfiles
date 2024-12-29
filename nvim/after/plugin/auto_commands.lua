b0273f6c81dbebd140bffde28291555abb656618
Sky Kosiner <sky@skykosiner.com>
2024-12-29 14:40:55 +0000

    format on save

--- nvim/after/plugin/auto_commands.lua
+++ nvim/after/plugin/auto_commands.lua
@@ -28,6 +28,23 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     command = [[%s/\s\+$//e]],
 })
 
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local c = vim.lsp.get_client_by_id(args.data.client_id)
+        if not c then return end
+
+        if vim.bo.filetype == "lua" then
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+                end,
+            })
+        end
+    end,
+})
+
 vim.api.nvim_create_autocmd("BufWritePost", {
     group = group,
     pattern = vim.fn.expand("$HOME/.dotfiles/.bookm"),
