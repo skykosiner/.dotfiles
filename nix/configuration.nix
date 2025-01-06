9b5cf418dd0d6ff610a91f798e1bc4a51be89711
Sky Kosiner <sky@skykosiner.com>
2025-01-06 12:32:46 +0000

    small chages to graphics stuf in nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -76,7 +76,7 @@ in {
 
     services.printing.enable = true;
 
-    hardware.pulseaudio.enable = false;
+    services.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
         enable = true;
