c49813f6f49ef05fa2586c382ce9b3cb08e212a2
Sky Kosiner <sky@skykosiner.com>
2025-05-20 08:48:28 +0100

    git create

--- nix/hosts/nix-btw/configuration.nix
+++ nix/hosts/nix-btw/configuration.nix
@@ -1,26 +1,27 @@
 { config, pkgs, ... }:
 
 {
-    imports = [
-        ../../configuration.nix
-            ./hardware-configuration.nix
-    ];
+  imports = [ ../../configuration.nix ./hardware-configuration.nix ];
 
-    networking.hostName = "nix-btw";
+  networking.hostName = "nix-btw";
 
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-    services.xserver.videoDrivers = [ "nvidia" ];
-
-    hardware = {
-        nvidia = {
-            package = config.boot.kernelPackages.nvidiaPackages.stable;
-            modesetting.enable = true;
-            open = true;
-            powerManagement.enable = true;
-            nvidiaSettings = true;
-        };
-        graphics = {
-            enable = true;
-        };
+  hardware = {
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
+      open = true;
+      powerManagement.enable = true;
+      nvidiaSettings = true;
     };
+    graphics = { enable = true; };
+  };
+
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "0 10 * * 1 /home/sky/.local/bin/updateServers"
+    ];
+  };
 }
