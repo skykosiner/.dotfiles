74fe31607af3af3f28f226ff5dd7fb89de87c218
Sky Kosiner <sky@skykosiner.com>
2026-04-17 06:19:26 +0100

    macos areoscpae

--- nix/mac.nix
+++ nix/mac.nix
@@ -63,7 +63,7 @@ in {
     enable = true;
 
     taps = [ ];
-    brews = [ "cowsay" "htop" "pass" "neovim-remote"];
+    brews = [ "cowsay" "htop" "pass" "neovim-remote" ];
     casks = [ "handbrake-app" "gimp" "calibre" "todoist-app" "raycast" ];
   };
 
@@ -93,11 +93,61 @@ in {
     TX-02
   ];
 
-  # system.activationScripts.applications.text = lib.mkForce ''
+  services.aerospace = {
+    enable = true;
+    settings = {
+      gaps = {
+        outer.left = 0;
+        outer.bottom = 0;
+        outer.top = 0;
+        outer.right = 0;
+      };
+      mode.main.binding = {
+        alt-j = "focus left";
+        alt-c = "focus down";
+        alt-k = "focus up";
+        alt-p = "focus right";
+
+        alt-shift-j = "move left";
+        alt-shift-c = "move down";
+        alt-shift-k = "move up";
+        alt-shift-p = "move right";
+
+        alt-minus = "resize smart -50";
+        alt-equal = "resize smart +50";
+
+        alt-enter = "exec-and-forget open -n /etc/profiles/per-user/sky/bin/alacritty";
+
+        alt-1 = "workspace 1";
+        alt-2 = "workspace 2";
+        alt-3 = "workspace 3";
+        alt-4 = "workspace 4";
+        alt-5 = "workspace 5";
+        alt-6 = "workspace 6";
+        alt-7 = "workspace 7";
+        alt-8 = "workspace 8";
+        alt-9 = "workspace 9";
+        alt-0 = "workspace 0";
+
+        alt-shift-1 = "move-node-to-workspace 1";
+        alt-shift-2 = "move-node-to-workspace 2";
+        alt-shift-3 = "move-node-to-workspace 3";
+        alt-shift-4 = "move-node-to-workspace 4";
+        alt-shift-5 = "move-node-to-workspace 5";
+        alt-shift-6 = "move-node-to-workspace 6";
+        alt-shift-7 = "move-node-to-workspace 7";
+        alt-shift-8 = "move-node-to-workspace 8";
+        alt-shift-9 = "move-node-to-workspace 9";
+        alt-shift-0 = "move-node-to-workspace 0";
+      };
+    };
+  };
+
+  # system.activationScripts.applications.text = lib.mkForce ""
   #   echo "Setting up /Applications/Nix Apps" >&2
   #   appsSrc="/etc/profiles/per-user/sky/bin"
   #   baseDir="/Applications/Nix Apps"
   #   mkdir -p "$baseDir"
   #   ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$appsSrc" "$baseDir"
-  # '';
+  # "";
 }
